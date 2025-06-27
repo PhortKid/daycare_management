530eedb3dfdf3135ac426e06f51e380201509c25
PhortKid <middlephort@gmail.com>
2025-06-27 09:47:38 +0300

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -12,7 +12,7 @@
 
     <!-- Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="dashboard.php">
+        <a class="nav-link" href="/dashboard.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span>
         </a>
